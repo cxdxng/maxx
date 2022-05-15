8afc24dbfb4a0ac81b5e0ab564c91776b4791856
cxdxng <marlon.scheer@t-online.de>
2022-05-15 13:28:50 +0200

    Added changes

--- lib/gpioHandling.dart
+++ lib/gpioHandling.dart
@@ -2,6 +2,8 @@
 
 import 'dart:async';
 
+import 'package:flutter_gpiod/flutter_gpiod.dart';
+
 class GpioHandling{
   GpioHandling._privateConstructor();
   static final GpioHandling instance = GpioHandling._privateConstructor();
@@ -13,5 +15,24 @@ class GpioHandling{
       print(DateTime.now());
     });
 
-  } 
+  }
+
+  void testingGPIO()async{
+    final chip = FlutterGpiod.instance.chips.singleWhere((chip) => chip.label == 'pinctrl-bcm2835');
+    final line = chip.lines[22];
+
+
+    // request it as input again, but this time we're also listening
+    // for edges; both in this case.
+    line.requestInput(consumer: "test",triggers: {SignalEdge.falling, SignalEdge.rising});
+
+    // line.onEvent will not emit any events if no triggers
+    // are requested for the line.
+    // this will run forever
+    await for (final event in line.onEvent) {
+      print("got GPIO line signal event: $event");
+    }
+
+    line.release();
+  }
 }
